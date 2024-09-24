7eeeb10edd12feede23012d12eb4d9f19bae047d
RidhamRj <singhal2003yash@gmail.com>
2024-09-24 10:12:11 +0530

    fixed recent file section issues

--- frontend/src/components/hero/HeroSec.jsx
+++ frontend/src/components/hero/HeroSec.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './Header'
+import RecentFiles from './RecentFiles'
 
 function HeroSec() {
   return (
     <div className='bg-heroBlack font-Montserrat text-customWhite  h-screen w-[84vw] '>
       <Header/>
+      <RecentFiles/>
     </div>
   )
 }
