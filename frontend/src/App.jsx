ea726868c9e8c3440453549f98653ac4e23f297e
RidhamRj <singhal2003yash@gmail.com>
2024-09-27 21:30:39 +0530

    integrated landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Workspace from './pages/workspace'
 import LandingPage from './pages/landingPage'
+import Signup from './pages/Signup'
 function App() {
 
   return (
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<LandingPage/>} />
+        <Route path="/signup" element={<Signup/>} />
         <Route path='/workspace' element={<Workspace/>} />
       </Routes>
     </BrowserRouter>
--- frontend/src/pages/landingPage.jsx
+++ frontend/src/pages/landingPage.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Navbar from "../components/landing page/Navbar";
+import Header from "../components/landing page/Header";
+import Features from "../components/landing page/Features";
+import VideoSection from "../components/landing page/VideoSection";
+import Footer from "../components/landing page/Footer";
 
 function LandingPage() {
   return (
     <div>
-      <Link to={'/workspace'}>
-        Workspace
-      </Link>
+      <Navbar/>
+      <Header/>
+      <Features/>
+      <div id="watch-demo-section">
+        <VideoSection/>
+      </div>
+      <Footer/>
     </div>
-  )
+  );
 }
 
-export default LandingPage
+export default LandingPage;
